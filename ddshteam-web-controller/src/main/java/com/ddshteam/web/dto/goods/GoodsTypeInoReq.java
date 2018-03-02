20f309420cab76c6e97bd3d8413eefe2fd4d18d0
arpgate <lunchao_110@163.com>
2018-03-03 01:39:35 +0800

    物资常量及物资工具类服务

---
+++ ddshteam-web-controller/src/main/java/com/ddshteam/web/dto/goods/GoodsTypeInoReq.java
@@ -0,0 +1,34 @@
+package com.ddshteam.web.dto.goods;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class GoodsTypeInoReq {
+	@NotBlank
+	String name;
+	@NotNull
+	String remark;
+	@NotNull
+	String parentid;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getParentid() {
+		return parentid;
+	}
+	public void setParentid(String parentid) {
+		this.parentid = parentid;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+}
