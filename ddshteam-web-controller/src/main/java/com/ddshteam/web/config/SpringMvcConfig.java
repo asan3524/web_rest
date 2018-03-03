81731342a21323d0902b4714a5d2d0940a575928
arpgate <lunchao_110@163.com>
2018-03-03 18:09:17 +0800

    测试合并

--- ddshteam-web-controller/src/main/java/com/ddshteam/web/config/SpringMvcConfig.java
+++ ddshteam-web-controller/src/main/java/com/ddshteam/web/config/SpringMvcConfig.java
@@ -188,6 +188,7 @@ public class SpringMvcConfig extends WebMvcConfigurerAdapter{
     @Bean  
     public WebMvcConfigurer corsConfigurer() {  
         return new WebMvcConfigurerAdapter() {  
+        	
             @Override  
             public void addCorsMappings(CorsRegistry registry) {  
                 registry.addMapping("/**")  
