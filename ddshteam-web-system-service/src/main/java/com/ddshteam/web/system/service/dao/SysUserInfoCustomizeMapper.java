55fcb83722aa4e5761b07b08f49bf4191f8b359f
arpgate <lunchao_110@163.com>
2018-03-05 17:57:13 +0800

    系统管理重构，完成基础dao/mapper的生成，与部分错误修正，待与原dao/mapper进一步业务完善

---
+++ ddshteam-web-system-service/src/main/java/com/ddshteam/web/system/service/dao/SysUserInfoCustomizeMapper.java
@@ -0,0 +1,42 @@
+package com.ddshteam.web.system.service.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.ddshteam.web.system.service.api.model.SysUserInfo;
+
+
+@Repository
+//@Mapper
+public interface SysUserInfoCustomizeMapper {
+	
+	public List<SysUserInfo> getUserList(@Param("name") String name, @Param("depId") String depId);
+	
+	public SysUserInfo getUserByAccount(@Param("account") String account);
+	
+	public SysUserInfo getUserById(@Param("userId") String userId);
+	
+	/**
+	 * <p>注意开启mysql驱动批量执行开关</p>
+	 * @param sysUser
+	 * @param roleIds 
+	 * @return
+	 */
+	public Integer saveUser(SysUserInfo sysUser, @Param("roleIds") String... roleIds);
+	
+	public Integer saveUserNoRole(SysUserInfo sysUser);
+	
+	public Integer updateUser(SysUserInfo sysUser, @Param("roleIds") String... roleIds);
+	
+	public Integer updateUserNoRole(SysUserInfo sysUser);
+	
+	public Integer updatePassword(
+			@Param("userId") String userId, 
+			@Param("newPassword") String newPassword);
+	
+	public Integer deleteUser(@Param("userId") String userId);
+	
+	public Integer setUserRole(@Param("userId") String userId, @Param("roleIds") String... roleIds);
+}
