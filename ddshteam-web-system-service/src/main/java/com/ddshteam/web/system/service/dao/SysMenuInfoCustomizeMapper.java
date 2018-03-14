bc51ad87c49255faa12bff00a224b9d0f3de068b
lishibang <lsb3524@163.com>
2018-03-14 21:09:24 +0800

    更新菜单状态树功能

--- ddshteam-web-system-service/src/main/java/com/ddshteam/web/system/service/dao/SysMenuInfoCustomizeMapper.java
+++ ddshteam-web-system-service/src/main/java/com/ddshteam/web/system/service/dao/SysMenuInfoCustomizeMapper.java
@@ -9,17 +9,66 @@ import com.ddshteam.web.system.service.api.model.SysMenuInfo;
 
 public interface SysMenuInfoCustomizeMapper {
 
+	/**
+	 * 获取用户的导航菜单
+	 * @Title: getMenusByUser
+	 * @param userId
+	 * @return List<SysMenuInfo>
+	 * @author lishibang
+	 */
 	List<SysMenuInfo> getMenusByUser(String userId);
 
+	/**
+	 * 获取所有菜单，给内置账户使用
+	 * @Title: getMenuTree
+	 * @return List<Tree>
+	 * @author lishibang
+	 */
+	List<Tree> getMenuTree();
+	
+	/**
+	 * 获取用户权限清单
+	 * @Title: getPermissionByUser
+	 * @param userId
+	 * @return List<String>
+	 * @author lishibang
+	 */
 	List<String> getPermissionByUser(String userId);
 
+	/**
+	 * 指定角色且指定账户的菜单权限
+	 * @Title: getMenuTreeByUserRole
+	 * @param userId
+	 * @param roleId
+	 * @return List<Tree>
+	 * @author lishibang
+	 */
 	List<Tree> getMenuTreeByUserRole(@Param("userId") String userId, @Param("roleId") String roleId);
 
+	/**
+	 * 指定角色菜单权限，给内置账户使用
+	 * @Title: getMenuTreeByRole
+	 * @param roleId
+	 * @return List<Tree>
+	 * @author lishibang
+	 */
 	List<Tree> getMenuTreeByRole(String roleId);
 
+	/**
+	 * 获取指定用户所有菜单，用于新增角色
+	 * @Title: getMenuTreeByUser
+	 * @param userId
+	 * @return List<Tree>
+	 * @author lishibang
+	 */
 	List<Tree> getMenuTreeByUser(String userId);
 
-	List<Tree> getMenuTree();
-
+	/**
+	 * 关联删除菜单
+	 * @Title: deleteByPrimaryKey
+	 * @param id
+	 * @return int
+	 * @author lishibang
+	 */
 	int deleteByPrimaryKey(String id);
 }
