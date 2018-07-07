cb7d2ee750e1bde08085442496b27d559e2f164e
arpgate <lunchao_110@163.com>
2018-07-07 23:15:03 +0800

    单文件上传

--- ddshteam-web-controller/src/main/java/com/ddshteam/web/controller/util/FileUtilController.java
+++ ddshteam-web-controller/src/main/java/com/ddshteam/web/controller/util/FileUtilController.java
@@ -1,6 +1,5 @@
 package com.ddshteam.web.controller.util;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -43,29 +42,29 @@ import io.swagger.annotations.ApiOperation;
 @Api(value = "/util/file", description = "文件-工具类")
 @RestController
 @RequestMapping(value = "/util/file")
-public class FileUtilController  extends BaseController{
+public class FileUtilController extends BaseController {
 	private final static Logger logger = LoggerFactory.getLogger(FileUtilController.class);
 
-	
 	@Reference(version = "1.0.0")
 	IFileService fileService;
-	
+
 	@ApiOperation(value = "文件上传", notes = "文件上传")
-	@PostMapping(value = { "/upload" })
+	@PostMapping(value = { "/uploads" })
 	@RequiresPermissions(UtilContants.Permission.PERMISSION_FILE_UPLOAD)
-	public Object fileUpload(@RequestParam MultipartFile[] upload,@RequestParam String reqdata,BindingResult errors) {
+	public Object fileUploads(@RequestParam MultipartFile[] upload, @RequestParam String reqdata,
+			BindingResult errors) {
 		logger.debug("FileUtilController.fileUpload()");
-		
-		if(StringUtils.isEmpty(reqdata)) {
+
+		if (StringUtils.isEmpty(reqdata)) {
 			logger.error("reqdata is null.");
 			return getResponse(HttpCode.BAD_REQUEST, false);
 		}
-		if (upload==null||errors.hasErrors()||upload.length<1) {
+		if (upload == null || errors.hasErrors() || upload.length < 1) {
 			String msg = errors.getAllErrors().get(0).getDefaultMessage();
 			logger.error(msg);
 			return getResponse(HttpCode.BAD_REQUEST, false, msg);
 		}
-		List<FileUploadReqData> reqdatas=(List<FileUploadReqData>) JSON.parse(reqdata);
+		List<FileUploadReqData> reqdatas = (List<FileUploadReqData>) JSON.parse(reqdata);
 
 		Subject subject = SecurityUtils.getSubject();
 		SysUserInfo user = (SysUserInfo) subject.getPrincipals().getPrimaryPrincipal();
@@ -73,18 +72,16 @@ public class FileUtilController  extends BaseController{
 		if (StringUtils.isEmpty(user)) {
 			return getResponse(HttpCode.UNAUTHORIZED, false);
 		}
-		
-		List<FileInfo> fileInfos=new ArrayList<FileInfo>();
-		int i=0;
-		for(MultipartFile multipartFile:upload)
-		{
-			if(multipartFile.getSize()>0)
-			{
-				FileUploadReqData fileData=reqdatas.get(i);
+
+		List<FileInfo> fileInfos = new ArrayList<FileInfo>();
+		int i = 0;
+		for (MultipartFile multipartFile : upload) {
+			if (multipartFile.getSize() > 0) {
+				FileUploadReqData fileData = reqdatas.get(i);
 				i++;
-				String fileName=multipartFile.getOriginalFilename();
+				String fileName = multipartFile.getOriginalFilename();
 				fileData.setFileName(fileName);
-				File file=new File(getRealPath(fileData));
+				File file = new File(getRealPath(fileData));
 				try {
 					multipartFile.transferTo(file);
 				} catch (IllegalStateException e) {
@@ -94,12 +91,12 @@ public class FileUtilController  extends BaseController{
 					logger.error(e.getMessage());
 					return getResponse(HttpCode.BAD_REQUEST, false, e.getMessage());
 				}
-				
- 				FileInfo fileInfo=new FileInfo();
+
+				FileInfo fileInfo = new FileInfo();
 				fileInfo.setBussnessObjId(fileData.getBussnessObjId());
 				fileInfo.setFilename(fileName);
 				fileInfo.setPath(file.getPath());
-				fileInfo.setFileSize(multipartFile.getSize()/1024/1024);
+				fileInfo.setFileSize(multipartFile.getSize() / 1024 / 1024);
 				fileInfo.setFileType(fileData.getFileType());
 				fileInfo.setStatus(UtilContants.Status.EFFECT);
 				fileInfo.setTableName(fileData.getAttType());
@@ -109,97 +106,149 @@ public class FileUtilController  extends BaseController{
 				fileInfos.add(fileInfo);
 			}
 		}
-		
-		List<FileInfo> result=fileService.addFileInfo(fileInfos);
+
+		List<FileInfo> result = fileService.addFileInfo(fileInfos);
 		return getResponse(result);
 	}
-	
-	
+
+	@ApiOperation(value = "文件上传", notes = "文件上传")
+	@PostMapping(value = { "/upload" })
+	@RequiresPermissions(UtilContants.Permission.PERMISSION_FILE_UPLOAD)
+	public Object fileUpload(@RequestParam MultipartFile upload, @RequestParam String reqdata, BindingResult errors) {
+		logger.debug("FileUtilController.fileUpload()");
+
+		if (StringUtils.isEmpty(reqdata)) {
+			logger.error("reqdata is null.");
+			return getResponse(HttpCode.BAD_REQUEST, false);
+		}
+		if (upload == null || errors.hasErrors()) {
+			String msg = errors.getAllErrors().get(0).getDefaultMessage();
+			logger.error(msg);
+			return getResponse(HttpCode.BAD_REQUEST, false, msg);
+		}
+		FileUploadReqData fileData = (FileUploadReqData) JSON.parse(reqdata);
+
+		Subject subject = SecurityUtils.getSubject();
+		SysUserInfo user = (SysUserInfo) subject.getPrincipals().getPrimaryPrincipal();
+
+		if (StringUtils.isEmpty(user)) {
+			return getResponse(HttpCode.UNAUTHORIZED, false);
+		}
+
+		List<FileInfo> fileInfos = new ArrayList<FileInfo>();
+
+		String fileName = upload.getOriginalFilename();
+		fileData.setFileName(fileName);
+		File file = new File(getRealPath(fileData));
+		try {
+			upload.transferTo(file);
+		} catch (IllegalStateException e) {
+			logger.error(e.getMessage());
+			return getResponse(HttpCode.BAD_REQUEST, false, e.getMessage());
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+			return getResponse(HttpCode.BAD_REQUEST, false, e.getMessage());
+		}
+
+		FileInfo fileInfo = new FileInfo();
+		fileInfo.setBussnessObjId(fileData.getBussnessObjId());
+		fileInfo.setFilename(fileName);
+		fileInfo.setPath(file.getPath());
+		fileInfo.setFileSize(upload.getSize() / 1024 / 1024);
+		fileInfo.setFileType(fileData.getFileType());
+		fileInfo.setStatus(UtilContants.Status.EFFECT);
+		fileInfo.setTableName(fileData.getAttType());
+		fileInfo.setType(fileData.getAttSubType());
+		fileInfo.setUserid(user.getId());
+		fileInfo.setUsername(user.getName());
+		fileInfos.add(fileInfo);
+
+		List<FileInfo> result = fileService.addFileInfo(fileInfos);
+		return getResponse(result);
+	}
+
 	@ApiOperation(value = "文件下载", notes = "文件下载")
 	@PostMapping(value = { "/download" })
 	@RequiresPermissions(UtilContants.Permission.PERMISSION_FILE_DOWNLOAD)
-	public Object filedownload(@RequestParam FileUploadReqData reqdata,HttpServletResponse respone, BindingResult errors) {
+	public Object filedownload(@RequestParam FileUploadReqData reqdata, HttpServletResponse respone,
+			BindingResult errors) {
 		logger.debug("FileUtilController.filedownload()");
 		if (errors.hasErrors()) {
 			String msg = errors.getAllErrors().get(0).getDefaultMessage();
 			logger.error(msg);
 			return getResponse(HttpCode.BAD_REQUEST, false, msg);
 		}
-		File file=new File(getRealPath(reqdata));
-		if(!file.exists())
-		{
+		File file = new File(getRealPath(reqdata));
+		if (!file.exists()) {
 			logger.error("文件不存在!");
 			return getResponse(HttpCode.NOT_FOUND, false, "文件不存在!");
 		}
-		
+
 		try {
 			respone.getOutputStream().write(FileUtils.readFileToByteArray(file));
 		} catch (IOException e) {
 			logger.error(e.getMessage());
 			return getResponse(HttpCode.INTERNAL_SERVER_ERROR, false, e.getMessage());
 		}
- 
+
 		return getResponse(HttpCode.OK, true);
 	}
-	
-	
+
 	@ApiOperation(value = "获取文件列表", notes = "获取文件列表")
 	@PostMapping(value = { "/fileinolist" })
 	@RequiresPermissions(UtilContants.Permission.PERMISSION_FILE_LIST)
-	public Object fileList(@RequestParam FileInfo FileInfo,@PageableDefault(page = 1, size = 10) Pageable pageable, BindingResult errors) {
+	public Object fileList(@RequestParam FileInfo FileInfo, @PageableDefault(page = 1, size = 10) Pageable pageable,
+			BindingResult errors) {
 		logger.debug("FileUtilController.fileList()");
 		if (errors.hasErrors()) {
 			String msg = errors.getAllErrors().get(0).getDefaultMessage();
 			logger.error(msg);
 			return getResponse(HttpCode.BAD_REQUEST, false, msg);
 		}
-		PageInfo<com.ddsh.util.service.api.data.FileInfo> fileInfos=fileService.getFileInfo(FileInfo,pageable.getPageNumber(), pageable.getPageSize());
+		PageInfo<com.ddsh.util.service.api.data.FileInfo> fileInfos = fileService.getFileInfo(FileInfo,
+				pageable.getPageNumber(), pageable.getPageSize());
 		return getResponse(fileInfos);
 	}
-	
+
 	@ApiOperation(value = "获取文件信息", notes = "获取文件信息")
 	@GetMapping(value = { "/fileinfo/{objid}" })
 	@RequiresPermissions(UtilContants.Permission.PERMISSION_FILE_INFO)
 	public Object fileinfo(@PathVariable("objid") String objid) {
 		logger.debug("FileUtilController.fileinfo()");
-		if(StringUtils.isEmpty(objid)) {
+		if (StringUtils.isEmpty(objid)) {
 			logger.error("objid is null.");
 			return getResponse(HttpCode.BAD_REQUEST, false);
 		}
-		
-		FileInfo fileInfo=fileService.getFileInfoByid(objid);
-		
- 		return getResponse(fileInfo);
+
+		FileInfo fileInfo = fileService.getFileInfoByid(objid);
+
+		return getResponse(fileInfo);
 	}
-	
+
 	@ApiOperation(value = "获取文件信息", notes = "获取文件信息")
 	@GetMapping(value = { "/del/{objid}" })
 	@RequiresPermissions(UtilContants.Permission.PERMISSION_FILE_DEL)
 	public Object fileDel(@PathVariable("objid") String objid) {
 		logger.debug("FileUtilController.fileDel()");
-		if(StringUtils.isEmpty(objid)) {
+		if (StringUtils.isEmpty(objid)) {
 			logger.error("objid is null.");
 			return getResponse(HttpCode.BAD_REQUEST, false);
 		}
-		
-		FileInfo fileInfo=fileService.getFileInfoByid(objid);
-		if(fileInfo.getPath()!=null)
-		{
-			File file=new File(fileInfo.getPath());
-			if(file.exists())
-			{
-				if(file.delete())
-				{
+
+		FileInfo fileInfo = fileService.getFileInfoByid(objid);
+		if (fileInfo.getPath() != null) {
+			File file = new File(fileInfo.getPath());
+			if (file.exists()) {
+				if (file.delete()) {
 					fileService.delFile(fileInfo.getId());
 				}
 			}
 		}
- 		return getResponse(fileInfo);
+		return getResponse(fileInfo);
 	}
-	
-	
-	private String getRealPath(FileUploadReqData reqdata)
-	{
-		return UtilContants.Sysset.UPLOAD_ROOT_PATH+File.pathSeparator+reqdata.getType()+File.pathSeparator+reqdata.getBussnessObjId()+File.pathSeparator+reqdata.getFileName();
+
+	private String getRealPath(FileUploadReqData reqdata) {
+		return UtilContants.Sysset.UPLOAD_ROOT_PATH + File.pathSeparator + reqdata.getType() + File.pathSeparator
+				+ reqdata.getBussnessObjId() + File.pathSeparator + reqdata.getFileName();
 	}
 }
